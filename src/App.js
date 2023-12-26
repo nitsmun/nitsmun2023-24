14602a88d9e9dd7c335bfdc8c961837e8dc03fe5
Kaushik-4 <kaushiktalukdar033@gmail.com>
2023-12-26 21:49:05 +0530

    letters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Letter from "./componets/letters";
+import Nitsmun from "./componets/nitsMun";
+import "./App.css"
+import Footer from "./componets/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <div>
+      <div className="letter-container" >
+       <Letter title="Secratory General" name="Maruf Padaya" src="https://res.cloudinary.com/dxcqxo6kl/image/upload/v1703446281/image_1_3_azcue6.png"/>
+       <Letter title="Faculty Advisor" name="Dr. Wasim Arif" src="https://res.cloudinary.com/dxcqxo6kl/image/upload/v1703446281/image_1_3_azcue6.png"/>
+       </div>
+       <Nitsmun/>
+       <Footer/>
     </div>
   );
-}
+};
+
+
 
 export default App;
+
